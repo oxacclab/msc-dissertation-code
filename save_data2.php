4edf812e6a0f05285175d8cd4a7091c62d139119
nikbpetrov <nikbpetrov@gmail.com>
2021-06-19 11:27:55 +0100

    Update save_data2.php

--- save_data2.php
+++ save_data2.php
@@ -2,18 +2,18 @@
     // the $_POST[] array will contain the passed in filename and data
     // the directory "data" is writable by the server (chmod 777)
     // the file path needs to be adjusted accordingly!
-    $filename = "data/".$_POST['filename'];
+    $filename = $_POST['filename'];
     $data = $_POST['filedata'];
     function crash_and_burn($reason) {
       http_response_code(500);
-      die("{error: \"$reason\", message: \"\", postdata: ".json_encode($_POST)."}");
+      die("{error: \"$reason\", message: \"\", filename: \"$filename\"}");
     }
     // Because the above came from the participant's machine, we sanitize the input
     if(!preg_match("/^[0-9a-zA-Z]+$/", $filename)) {
         http_response_code(400);
         crash_and_burn("Invalid filename");
     } else {
-      $filename = $filename.".csv";
+      $filename = "data/$filename.csv";
     }
     // write the file to disk
     if(file_put_contents($filename, $data) === false) {
